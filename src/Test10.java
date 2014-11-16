82ce211cda1fff2be1370112be3db523bf4af124
santoshfaculty1 <nouser@noemail.com>
2014-11-16 21:02:56 +0530

    Initial Draft

---
+++ src/Test10.java
@@ -0,0 +1,14 @@
+/**
+ * Main class
+ */
+public class Test10 {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {    	
+		System.out.println("Hello world");
+	
+    }
+    //New QUestion Here.
+}
